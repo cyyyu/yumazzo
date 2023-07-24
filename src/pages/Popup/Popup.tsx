f017a3e63d929d37a12729b18b54b1c78bbd1bdb
Chuang Yu <cyu9960@gmail.com>
2023-07-24 18:47:10 +0800

    Implement basic RecipeDescription UI

--- src/pages/Popup/Popup.tsx
+++ src/pages/Popup/Popup.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import SearchBar from './components/SearchBar';
 import Toolbar from './components/Toolbar';
+import RecipeDescription from './components/RecipeDescription';
 
 const Popup = () => {
   return (
     <div className="w-full h-screen min-h-fit bg-[#0D1119] p-6">
       <SearchBar />
       <Toolbar />
+      <RecipeDescription />
     </div>
   );
 };
