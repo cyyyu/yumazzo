cc74af6d8a1e5d3656110d2ccb9cfc08b90b437c
Chuang Yu <cyu9960@gmail.com>
2023-07-24 17:53:08 +0800

    Impl basic SearchBar UI

--- src/pages/Popup/components/SearchBar.tsx
+++ src/pages/Popup/components/SearchBar.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import SearchIcon from '../../../assets/img/Search';
 
 export default function SearchBar() {
-  return <div className="text-white">SearchBar</div>;
+  return (
+    <div className="w-full py-[8px] px-[11px] rounded-md bg-[#131823] flex shadow-[0px_0px_0px_1px_#5b6178]">
+      <img src={SearchIcon} className="w-5 h-5 mr-2.5" />
+      <input
+        type="text"
+        placeholder="Search cuisine"
+        className="text-white bg-[#131823] outline-none grow"
+      />
+    </div>
+  );
 }
